e2201e852890f95a7edd004a72eefe308530f68f
wordhulhasan <wordh.u.hasan@ieee.org>
2023-09-27 22:56:28 -0500

    first commit

---
+++ src/main/java/com/example/aicybersecurityedu/AicybersecurityeduApplication.java
@@ -0,0 +1,13 @@
+package com.example.aicybersecurityedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicybersecurityeduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AicybersecurityeduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aicybersecurityedu/AicybersecurityeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aicybersecurityedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicybersecurityeduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
