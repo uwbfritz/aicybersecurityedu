bdfbe4e6c6d3f7b5395b5e36b4025835e9a3392f
Kimia Tuz Zaman <kimia.t.zaman@ieee.org>
2023-11-07 20:43:56 -0600

    basic page structure created

--- src/main/java/com/example/aicybersecurityedu/controller/PageViewController.java
+++ src/main/java/com/example/aicybersecurityedu/controller/PageViewController.java
@@ -1,7 +1,12 @@
 package com.example.aicybersecurityedu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class PageViewController {
@@ -9,4 +14,52 @@ public class PageViewController {
     public String home() {
         return "home"; // Returns the home view
     }
+
+    @GetMapping("/spam-email")
+    public String spamEmailDetectionPage() {
+        return "spam-email";
+    }
+
+    @PostMapping("/check-spam")
+    public String checkSpam(@RequestParam String emailContent, Model model) {
+        // In a real scenario, you would use a Machine Learning model to check for spam.
+        // For this demo, we will just return a hardcoded response.
+
+        model.addAttribute("isSpam", true); // This is just for the demo
+        return "spam-email";
+    }
+
+    @GetMapping("/facial-recognition")
+    public String facialRecognitionPage() {
+        return "facial-recognition";
+    }
+
+    @PostMapping("/detect-face")
+    public String detectFace(MultipartFile faceImage, Model model) {
+        // In a real scenario, you'd use a facial recognition API or model to detect faces.
+        // For this demo, we'll just return a hardcoded response.
+        model.addAttribute("faceDetected", true); // This is just for the demo
+        return "facial-recognition";
+    }
+
+    @GetMapping("/{topic}")
+    public String loadTopicContent(@PathVariable String topic, Model model) {
+        // Placeholder content. In a real-world scenario, this data might be fetched from a database or other sources.
+        switch(topic) {
+            case "network-security":
+                model.addAttribute("topicTitle", "Network Security");
+                model.addAttribute("topicContent", "Network security is...");
+                break;
+            case "cloud-security":
+                model.addAttribute("topicTitle", "Cloud Security");
+                model.addAttribute("topicContent", "Cloud security is...");
+                break;
+            // Handle other topics similarly...
+            default:
+                model.addAttribute("topicTitle", "Topic not found");
+                model.addAttribute("topicContent", "The selected topic does not exist.");
+        }
+        return "education";
+    }
+
 }
